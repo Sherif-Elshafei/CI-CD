90326787b003317f075c1742d06d10c7a2f4e873
sherif <sherif>
2022-05-30 12:56:09 +0200

    test will be committed and run

--- circles.py
+++ circles.py
@@ -2,9 +2,12 @@ from math import pi
 
 #program to find the area of a circle
 def circle_area(radius):
+    if radius < 0:
+        raise ValueError("The radius cannot be negative.")
+    if type(radius) not in [float, int]:
+        raise TypeError("The radius must be of type float or integer")
     return pi*(radius**2)
 
-print(circle_area(10))
 
 
 
